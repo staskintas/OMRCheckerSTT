5c1f852e0cd87c9ce6fb77e4bba6afb8d04dc1ba
staskintas <staskintas@gmail.com>
2023-12-08 08:51:02 +0200

    two new field types added: horizontal digits and single choice question

--- src/constants.py
+++ src/constants.py
@@ -40,6 +40,14 @@ FIELD_TYPES = {
     #
     # You can create and append custom field types here-
     #
+
+    # Horizontal integers
+    "QTYPE_INTH": {
+    "bubbleValues": ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+    "direction": "horizontal",
+    },
+    # Single choice question
+    "QTYPE_SCQ": {"bubbleValues": ["1"], "direction": "horizontal"},
 }
 
 # TODO: move to interaction.py
